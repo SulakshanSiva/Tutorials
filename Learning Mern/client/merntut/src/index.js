1853e1ff0fa6b3746c88b8311a7dda91da4de07f
Sulakshan <sulakshansivak@gmail.com>
2023-01-28 11:54:30 -0500

    completed L6, redux

--- Learning Mern/client/merntut/src/index.js
+++ Learning Mern/client/merntut/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
         <Routes>
-            <Route path="/*" element={<App />} />
+          <Route path="/*" element={<App />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
