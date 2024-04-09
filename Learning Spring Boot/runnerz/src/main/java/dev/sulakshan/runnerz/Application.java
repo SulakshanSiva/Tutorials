6a93f545df7eaaad94bd7c2af01dda750276e9b4
Sulakshan Sivakumaran <sulakshansivak@gmail.com>
2024-04-08 20:36:12 -0400

    Completed module 3

--- Learning Spring Boot/runnerz/src/main/java/dev/sulakshan/runnerz/Application.java
+++ Learning Spring Boot/runnerz/src/main/java/dev/sulakshan/runnerz/Application.java
@@ -1,6 +1,5 @@
 package dev.sulakshan.runnerz;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -9,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import dev.sulakshan.runnerz.run.Location;
